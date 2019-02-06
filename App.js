151ad1ea80bec2cd559f93d0dbf40575ea3338c3
Dilip Poudel <dilippoudel578@gmail.com>
2019-02-07 01:44:43 +0200

    Exercise-4 Shopping list app

--- App.js
+++ App.js
@@ -1,95 +1,86 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
+import { StyleSheet, Alert, View, TextInput, Button, Text } from 'react-native';
+import ListItem from './components/listItem';
 
 export default class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { firstInput: '', secondInput: '', results: '', data: [] };
-	}
-
-	addNumber = () => {
-		this.setState({ results: parseInt(this.state.firstInput) + parseInt(this.state.secondInput) });
-		const result = parseInt(this.state.firstInput) + parseInt(this.state.secondInput);
-		const text = `${this.state.firstInput} + ${this.state.secondInput}  =  ${result}`;
-		this.setState({ data: [...this.state.data, { key: text }] });
-	};
-	subtractNumber = () => {
-		this.setState({ results: parseInt(this.state.firstInput) - parseInt(this.state.secondInput) });
-		const result = parseInt(this.state.firstInput) - parseInt(this.state.secondInput);
-		const text = `${this.state.firstInput} - ${this.state.secondInput}  =  ${result}`;
-		this.setState({ data: [...this.state.data, { key:text }] });
-	};
-	render() {
-		return (
-			<View style={styles.container}>
-				<View>
-					<Text style={styles.text}>Calculator</Text>
-				</View>
-				<Text style={styles.text}>Result: {this.state.results}</Text>
-				<View />
-				<View>
-					<TextInput
-						style={styles.inputText}
-						onChangeText={firstInput => this.setState({ firstInput })}
-						value={this.state.firstInput}
-						keyboardType="numeric"
-					/>
-				</View>
-				<View>
-					<TextInput
-						style={styles.inputText}
-						onChangeText={secondInput => this.setState({ secondInput })}
-						value={this.state.secondInput}
-						keyboardType="numeric"
-					/>
-				</View>
-				<View>
-					<View style={styles.buttonText}>
-						<View style={styles.button}>
-							<Button onPress={this.addNumber} title="+" />
-						</View>
-						<View style={styles.button}>
-							<Button onPress={this.subtractNumber} title="-" />
-						</View>
-					</View>
-				</View>
-				<View>
-					<Text>History</Text>
-					<FlatList data={this.state.data} renderItem={({ item }) => <Text>{item.key}</Text>} />
-				</View>
-			</View>
-		);
-	}
+  state = {
+    placeName : '',
+    places: []
+  }
+  placeHandler = (val) => {
+    this.setState({
+      placeName: val
+    })
+  }
+  handleSubmit = () => {
+    if(this.state.placeName === ""){
+      Alert.alert("Add the item.")
+    }
+    else{
+      this.setState(prevState => {
+        return {
+          places: [...this.state.places, prevState.placeName]
+        }
+      })
+    }
+  }
+  handleDelete = () => {
+    this.setState({places: [], placeName:""})
+  }
+  render() {
+    const placeOutput = this.state.places.map((place,i) => (
+      <ListItem key = {i} placeName = {place}/>
+    ))
+    return (
+      <View style={styles.container}>
+      <View style = {styles.inputContainer}>
+        <TextInput 
+        style = {styles.placeInput}
+        onChangeText = {this.placeHandler}
+        placeholder = "Add Shopping Lists"
+        value = {this.state.placeName}/>
+        <Button 
+        style = {styles.placeButton} 
+        title = "Add"
+        onPress = {this.handleSubmit}/>
+         <Button 
+        style = {styles.placeButton} 
+        title = "Clear"
+        onPress = {this.handleDelete}/>
+      </View>
+      <View>
+        <Text style = {{fontSize: 20}}>Shopping Lists:</Text>
+      </View>
+      <View style = {styles.listContainer}>
+        {placeOutput}
+      </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		padding: 30,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'flex-start',
-	},
-	inputText: {
-		marginTop: 10,
-		height: 30,
-		width: 200,
-		padding: 5,
-		borderColor: 'gray',
-		borderWidth: 1,
-	},
-	text: {
-		fontSize: 25,
-	},
-	buttonText: {
-		marginTop: 10,
-		marginLeft: 10,
-		padding: 10,
-		flexDirection: 'row',
-		justifyContent: 'space-around',
-		backgroundColor: 'lightgray',
-	},
-	button: {
-		marginHorizontal: 20,
-	},
+  container: {
+    flex: 1,
+    padding: 30,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  inputContainer: {
+    //flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  placeInput: {
+    width: '70%'
+  },
+  placeButton: {
+    width: '30%'
+  },
+  listContainer: {
+   width: "100%",
+  }
 });
